f678c154d848e1ecdd33cc8b84a785e7da71cf37
Kotipalli Sai Phani Teja <donsai731@gmail.com>
2025-01-17 13:41:02 +0530

    updated the code analysis functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,16 @@
-// .eslintrc.js
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module'
-    },
-    rules: {
-      'semi': ['error', 'always'],
-      'quotes': ['error', 'single'],
-      'no-unused-vars': 'warn',
-      'no-console': 'warn'
-    }
-  };
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    "no-unused-vars": "warn",
+    "no-console": "warn",
+  },
+};
